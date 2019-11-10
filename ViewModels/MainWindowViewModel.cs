5d937379b8257490b4ab75ca6e4910bb2c6eb7db
Kamil Paczkowski <37217644+ShamanDev@users.noreply.github.com>
2019-11-10 21:08:49 +0100

    Add files via upload

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,14 @@
+﻿using MusicPlayah.Models;
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace MusicPlayah.ViewModels
+{
+    public class MainWindowViewModel : ReactiveObject
+    { 
+        public List<Song> Songs => new List<Song>() { new Song() { Title="Bones - Mortuary", URL="U mom gei"}, new Song() { Title = "Pouya - Settle Down", URL = "hah" } };
+    }
+}
---
+++ Views/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+using System;
+
+namespace MusicPlayah.Views
+{
+    public class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+        }
+        private void InitializeComponent()
+        {
+            this.CanResize = false;
+            this.MaxHeight = 600;
+            this.MinHeight = 600;
+            this.MaxWidth = 500;
+            this.MinWidth = 500;
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        private void OnButtonClick(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("U mom gei!!!");
+        }
+    }
+}
